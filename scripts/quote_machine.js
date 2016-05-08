d0bdfd0d04e47921d7803b8362ac522b231838f0
4iar <4iar@users.noreply.github.com>
2016-05-08 18:54:37 +0100

    Add ajax quote getting function

--- scripts/quote_machine.js
+++ scripts/quote_machine.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-  $("#quote-text").text("quote text placeholder");
+    $("#quote-text").text("quote text placeholder");
+
+    $("#quote-refresh-button").click(function() {
+
+        $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", function(json) {
+          $("#quote-text").text(json.content);
+        });
+
+    });
+
 });
