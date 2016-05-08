1e234fc68743b7517aacf33ab621efe427817cd2
4iar <4iar@users.noreply.github.com>
2016-05-08 18:55:13 +0100

    Spin the refresh icon when loading

--- scripts/quote_machine.js
+++ scripts/quote_machine.js
@@ -2,11 +2,14 @@ $(document).ready(function() {
     $("#quote-text").text("quote text placeholder");
 
     $("#quote-refresh-button").click(function() {
+        // Make the refresh fa-icon spin
+        $("#refresh-spinner").addClass("fa-spin");
 
         $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", function(json) {
           $("#quote-text").text(json.content);
         });
 
+        $("#refresh-spinner").removeClass("fa-spin");
     });
 
 });
